2437bf2fddd1a51a8efa2624d358a75adb340bdb
theo_cha <cha_theo@hotmail.com>
2022-08-02 14:45:14 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import Books from './Books'
+import Books from './Books';
+import Books_Feed from './Books_Feed';
 
 function App() {
   return (
     <div>
       <h1>Books app</h1>
+      <Books_Feed />
       <Books />
     </div>
+
+    
   );
 }
 
--- src/Books_Feed.js
+++ src/Books_Feed.js
@@ -2,15 +2,15 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import {FaBeer} from "react-icons/fa"
 
-const Books = () => {
+const Books_Feed = () => {
   const [books, setBooks] = useState([]);
 
   useEffect(() => {
     const fetchBooks = async () => {
       const res = await axios.get(
-        `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${process.env.REACT_APP_BOOKS_API_KEY}`
+        `https://books-api-theo.herokuapp.com/books`
       );
-      setBooks(res.data.results.books);
+      setBooks(res.data);
       console.log(res.data);
     };
 
@@ -23,23 +23,28 @@ const Books = () => {
       <section className="grid grid-cols-1 gap-10 px-5 sm:grid-col-2 lg:grid-cols-3 xl:grid-col-4">
         {books.map((book) => {
           const {
-            author,
-            book_image,
-            buy_links,
-            description,
-            primary_isbn10,
-            publisher,
-            rank,
+            id,
+            isbn,
             title,
+            subtitle,
+            author,
+            published,
+            publisher,
+            pages,
+            description,
+            website
           } = book;
 
           return (
-            <article key={rank} className="bg-gray-100 py-5 px-10 rounded-lg sm:px-5">
+            <article key={id} className="bg-gray-100 py-5 px-10 rounded-lg sm:px-5">
               <div>
-                <img className="block mx-auto w-1/2" src={book_image} alt={title} />
+                <img className="block mx-auto w-1/2" src={title} alt={title} />
               </div>
               <div>
                 <h3 className="font-bold my-2 text-2xl">{title}</h3>
+              </div>
+              <div>
+                <h4 className="font-bold my-2 text-xl">{subtitle}</h4>
               </div>
               <div>
                 <p className="mb-4">{description}</p>
@@ -48,18 +53,11 @@ const Books = () => {
 
               <ul className="mb-4">
                  <li><span className="font-bold">Publisher:</span> {publisher}</li>
-                <li><span className="font-bold">ISBN:</span> {primary_isbn10}</li>
+                 <li><span className="font-bold">Pages:</span> {pages}</li>
+                <li><span className="font-bold">Website:</span> {website}</li>
+                <li><span className="font-bold">Published:</span> {published}</li>
+                <li><span className="font-bold">ISBN:</span> {isbn}</li>
                 
-              </ul>
-              
-              <ul>
-              <h3 className="font-bold text-xl">Buy now</h3>
-                {buy_links.map((link)=>{
-                  const {name, url} = link
-                  return (<div key={name}>
-                    <a href={url} className="flex item-center" target="_blank">{name} <FaBeer className="ml-1"/></a>
-                  </div>)
-                })}
               </ul>
             </article>
           );
@@ -70,4 +68,4 @@ const Books = () => {
   );
 };
 
-export default Books;
+export default Books_Feed;
