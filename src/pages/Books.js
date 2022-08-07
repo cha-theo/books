afb7242c9a0041389ae3fbf762d40be7353a0fed
theo_cha <cha_theo@hotmail.com>
2022-08-07 23:42:49 +0300

    fix single product

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -7,13 +7,65 @@ import "../index.css";
 export default function Books(props) {
   const books = props.books;
   const {bookid} = useParams()
-  const thisBook = books.find(book => book.id === bookid)
-  console.log(books)
-  return (
-      <div>
-          <h1>{thisBook.title}</h1>
-      </div>
-  )
+  const thisBook = books.find(book => book.id.toString() === bookid)
+  
+  return bookid > books.length - 1 ? (
+    <NotFound />
+  ) : (
+    <div>
+      <article
+        key={thisBook.id}
+        className="bg-gray-100 py-5 px-10 rounded-lg sm:px-5"
+      >
+        <div>
+          <img
+            className="block mx-auto w-1/2"
+            src={thisBook.image}
+            alt={thisBook.title}
+          />
+        </div>
+        <div>
+          <h3 className="font-bold my-2 text-2xl">{thisBook.title}</h3>
+        </div>
+        <div>
+          <h4 className="font-bold my-2 text-xl">{thisBook.subtitle}</h4>
+        </div>
+        <div>
+          <p className="mb-4">{thisBook.description}</p>
+          <p>
+            <span className="font-bold">Author:</span> {thisBook.author}
+          </p>
+        </div>
+
+        <ul className="mb-4">
+          <li>
+            <span className="font-bold">Publisher:</span>{" "}
+            {thisBook.publisher}
+          </li>
+          <li>
+            <span className="font-bold">Pages:</span> {thisBook.pages}
+          </li>
+          <li>
+            <span className="font-bold">ISBN:</span> {thisBook.isbn}
+          </li>
+          <li>
+            <span className="font-bold">Release Date:</span>{" "}
+            {thisBook.releaseDate}
+          </li>
+          <li>
+            <a
+              href={thisBook.website}
+              target="_blank"
+              rel="noreferrer"
+              className="font-bold"
+            >
+              Website
+            </a>
+          </li>
+        </ul>
+      </article>
+    </div>
+  );
 }
 
 
@@ -27,47 +79,47 @@ export default function Books(props) {
 //   ) : (
 //     <div>
 //       <article
-//         key={books[bookid].id}
+//         key={thisBook.id}
 //         className="bg-gray-100 py-5 px-10 rounded-lg sm:px-5"
 //       >
 //         <div>
 //           <img
 //             className="block mx-auto w-1/2"
-//             src={books[bookid].image}
-//             alt={books[bookid].title}
+//             src={thisBook.image}
+//             alt={thisBook.title}
 //           />
 //         </div>
 //         <div>
-//           <h3 className="font-bold my-2 text-2xl">{books[bookid].title}</h3>
+//           <h3 className="font-bold my-2 text-2xl">{thisBook.title}</h3>
 //         </div>
 //         <div>
-//           <h4 className="font-bold my-2 text-xl">{books[bookid].subtitle}</h4>
+//           <h4 className="font-bold my-2 text-xl">{thisBook.subtitle}</h4>
 //         </div>
 //         <div>
-//           <p className="mb-4">{books[bookid].description}</p>
+//           <p className="mb-4">{thisBook.description}</p>
 //           <p>
-//             <span className="font-bold">Author:</span> {books[bookid].author}
+//             <span className="font-bold">Author:</span> {thisBook.author}
 //           </p>
 //         </div>
 
 //         <ul className="mb-4">
 //           <li>
 //             <span className="font-bold">Publisher:</span>{" "}
-//             {books[bookid].publisher}
+//             {thisBook.publisher}
 //           </li>
 //           <li>
-//             <span className="font-bold">Pages:</span> {books[bookid].pages}
+//             <span className="font-bold">Pages:</span> {thisBook.pages}
 //           </li>
 //           <li>
-//             <span className="font-bold">ISBN:</span> {books[bookid].isbn}
+//             <span className="font-bold">ISBN:</span> {thisBook.isbn}
 //           </li>
 //           <li>
 //             <span className="font-bold">Release Date:</span>{" "}
-//             {books[bookid].releaseDate}
+//             {thisBook.releaseDate}
 //           </li>
 //           <li>
 //             <a
-//               href={books[bookid].website}
+//               href={thisBook.website}
 //               target="_blank"
 //               rel="noreferrer"
 //               className="font-bold"
@@ -80,43 +132,3 @@ export default function Books(props) {
 //     </div>
 //   );
 // }
-
-
-
-  // if (bookid > books.length-1){
-  //   return (
-  //     <NotFound />
-  //   )
-  // } else
-  // return (
-  //   <div>
-  //      <article key={books[bookid].id} className="bg-gray-100 py-5 px-10 rounded-lg sm:px-5">
-
-  //             <div>
-  //               <img className="block mx-auto w-1/2" src={books[bookid].image} alt={books[bookid].title} />
-
-  //             </div>
-  //             <div>
-
-  //               <h3 className="font-bold my-2 text-2xl">{books[bookid].title}</h3>
-
-  //             </div>
-  //             <div>
-  //               <h4 className="font-bold my-2 text-xl">{books[bookid].subtitle}</h4>
-  //             </div>
-  //             <div>
-  //               <p className="mb-4">{books[bookid].description}</p>
-  //               <p><span className="font-bold">Author:</span> {books[bookid].author}</p>
-  //             </div>
-
-  //             <ul className="mb-4">
-  //               <li><span className="font-bold">Publisher:</span> {books[bookid].publisher}</li>
-  //               <li><span className="font-bold">Pages:</span> {books[bookid].pages}</li>
-  //               <li><span className="font-bold">ISBN:</span> {books[bookid].isbn}</li>
-  //               <li><span className="font-bold">Release Date:</span> {books[bookid].releaseDate}</li>
-  //               <li><a href={books[bookid].website} target="_blank" rel="noreferrer" className="font-bold">Website</a></li>
-  //             </ul>
-
-  //           </article>
-  //   </div>
-  // )
