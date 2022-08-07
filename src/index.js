144d4ed8bea065a34ad2e36562e4a51c9fd27f87
theo_cha <cha_theo@hotmail.com>
2022-08-07 15:28:37 +0300

    fixes and start add book

--- src/index.js
+++ src/index.js
@@ -17,11 +17,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
-
-
-//    https://www.youtube.com/watch?v=1O496C-79Pg
-//    https://www.youtube.com/watch?v=7xL9c39DhjI
-//    https://www.youtube.com/watch?v=5Y5QKfxTErU
-// filtering: https://www.youtube.com/watch?v=MY6ZZIn93V8&t=5s
-// routers https://www.youtube.com/watch?v=Ul3y1LXxzdU 4:08
--- src/pages/NewBook.js
+++ src/pages/NewBook.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 
 export default function NewBook(props) {
-    
+    const books = props.books;
+    console.log(books)
     return <h1>New Book</h1>
 
   }
