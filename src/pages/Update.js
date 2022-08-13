5637dff2cfde5e94c39f760e3395978ca5185722
theo_cha <cha_theo@hotmail.com>
2022-08-13 03:27:06 +0300

    fix bootstrap update form

--- src/pages/Update.js
+++ src/pages/Update.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Button, Form } from "semantic-ui-react";
+import { Button, Form, Container, Col, Row } from "react-bootstrap";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
@@ -51,110 +51,267 @@ export default function Update() {
   }, []);
 
   return (
-    <Form>
-      <Form.Field>
-        <label>Title</label>
-        <input
-          name="btitle"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          placeholder="Title"
-        />
-      </Form.Field>
+    
+    
+    <Container>
+    <Row>
+      <Col>
+        <Form>
 
-      <Form.Field>
-        <label>Subtitle</label>
-        <input
-          name="bsubtitle"
-          value={subtitle}
-          onChange={(e) => setSubtitle(e.target.value)}
-          placeholder="Subtitle"
-        />
-      </Form.Field>
+          <Form.Group className="mb-3" controlId="btitle">
+            <Form.Label>Title</Form.Label>
+            <Form.Control 
+            type="text" 
+            placeholder="Enter the title"  
+            value={title}
+            onChange={(e) => setTitle(e.target.value)} />
+            <Form.Text className="text-muted">
+              Max 120 characters
+            </Form.Text>
+          </Form.Group>
 
-      <Form.Field>
-        <label>Author</label>
-        <input
-          name="bauthor"
-          value={author}
-          onChange={(e) => setAuthor(e.target.value)}
-          placeholder="Author"
-        />
-      </Form.Field>
 
-      <Form.Field>
-        <label>ISBN</label>
-        <input
-          name="bisbn"
-          value={isbn}
-          onChange={(e) => setISBN(e.target.value)}
-          placeholder="ISBN"
-        />
-      </Form.Field>
+          <Form.Group className="mb-3" controlId="bsubtitle">
+            <Form.Label>Subtitle</Form.Label>
+            <Form.Control 
+            type="text" 
+            placeholder="Enter the subtitle" 
+            value={subtitle} 
+            onChange={(e) => setSubtitle(e.target.value)} />
+            <Form.Text className="text-muted">
+              Max 160 characters
+            </Form.Text>
+          </Form.Group>
 
-      <Form.Field>
-        <label>Published</label>
-        <input
-          name="bpublished"
-          value={published.substring(0, 4)}
-          onChange={(e) => setPublished(e.target.value)}
-          placeholder="Published"
-        />
-      </Form.Field>
 
-      <Form.Field>
-        <label>Publisher</label>
-        <input
-          name="bpublisher"
-          value={publisher}
-          onChange={(e) => setPublisher(e.target.value)}
-          placeholder="Publisher"
-        />
-      </Form.Field>
+          <Form.Group className="mb-3" controlId="bauthor">
+            <Form.Label>Author</Form.Label>
+            <Form.Control 
+            type="text" 
+            placeholder="Enter the Author"  
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)} />
+            <Form.Text className="text-muted">
+              Max 3 Authors
+            </Form.Text>
+          </Form.Group>
 
-      <Form.Field>
-        <label>Image</label>
-        <input
-          name="bimage"
-          value={image}
-          onChange={(e) => setImage(e.target.value)}
-          placeholder="Image"
-        />
-      </Form.Field>
 
-      <Form.Field>
-        <label>Pages</label>
-        <input
-          name="bpages"
-          value={pages}
-          onChange={(e) => setPages(e.target.value)}
-          placeholder="Pages"
-        />
-      </Form.Field>
+          <Form.Group className="mb-3" controlId="bisbn">
+            <Form.Label>ISBN</Form.Label>
+            <Form.Control 
+            type="number" 
+            placeholder="Enter the ISBN"  
+            value={isbn}
+            onChange={(e) => setISBN(e.target.value)} />
+            <Form.Text className="text-muted">
+              10 digits number
+            </Form.Text>
+          </Form.Group>
 
-      <Form.Field>
-        <label>Description</label>
-        <input
-          name="bdescription"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          placeholder="Description"
-        />
-      </Form.Field>
+          <Form.Group className="mb-3" controlId="bpublished">
+            <Form.Label>Published</Form.Label>
+            <Form.Control 
+            type="number" 
+            placeholder="Enter the Published year"  
+            value={published.substring(0, 4)}
+            onChange={(e) => setPublished(e.target.value)} />
+            <Form.Text className="text-muted">
+              Only the year, not the full date
+            </Form.Text>
+          </Form.Group>
 
-      <Form.Field>
-        <label>Website</label>
-        <input
-          name="bwebsite"
-          value={website}
-          onChange={(e) => setWebsite(e.target.value)}
-          placeholder="Website"
-        />
-      </Form.Field>
+        </Form>
+      </Col>
 
-      <Button type="submit" onClick={sendDataToAPI}>
-        Update
-      </Button>
-    </Form>
+      <Col>
+        <Form>
+
+        <Form.Group className="mb-3" controlId="bpublisher">
+            <Form.Label>Publisher</Form.Label>
+            <Form.Control 
+            type="text" 
+            placeholder="Enter the Publisher"  
+            value={publisher}
+            onChange={(e) => setPublisher(e.target.value)} />
+            <Form.Text className="text-muted">
+            Max 60 characters
+            </Form.Text>
+          </Form.Group>
+
+
+
+          <Form.Group className="mb-3" controlId="bimage">
+            <Form.Label>Image</Form.Label>
+            <Form.Control 
+            type="text" 
+            placeholder="Enter the Image URL" 
+            value={image} 
+            onChange={(e) => setImage(e.target.value)} />
+            <Form.Text className="text-muted">
+            For example: https://picsum.photos/200/300
+            </Form.Text>
+          </Form.Group>
+
+
+          <Form.Group className="mb-3" controlId="bpages">
+            <Form.Label>Pages</Form.Label>
+            <Form.Control 
+            type="number" 
+            placeholder="Enter the number of the pages"  
+            value={pages}
+            onChange={(e) => setPages(e.target.value)} />
+            <Form.Text className="text-muted">
+            Max 4 digit number
+            </Form.Text>
+          </Form.Group>
+
+
+          <Form.Group className="mb-3" controlId="bdescription">
+            <Form.Label>Description</Form.Label>
+            <Form.Control 
+            type="text" 
+            placeholder="Enter the Description"  
+            value={description}
+            onChange={(e) => setDescription(e.target.value)} />
+            <Form.Text className="text-muted">
+            Max 512 characters
+            </Form.Text>
+          </Form.Group>
+      
+
+          <Form.Group className="mb-3" controlId="bwebsite">
+            <Form.Label>Website</Form.Label>
+            <Form.Control 
+            type="text" 
+            placeholder="Enter the Website" 
+            value={website} 
+            onChange={(e) => setWebsite(e.target.value)} />
+            <Form.Text className="text-muted">
+            For example: google.com
+            </Form.Text>
+          </Form.Group>
+
+
+        </Form>
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+      </Col>
+      <Col>
+      <Button className="float-end" variant="success" size="lg" type="submit"  onClick={sendDataToAPI}>
+      Update Book
+    </Button>
+      </Col>
+    </Row>
+  </Container>   
+    
+    
+    // <Form>
+    //   <Form.Field>
+    //     <label>Title</label>
+    //     <input
+    //       name="btitle"
+    //       value={title}
+    //       onChange={(e) => setTitle(e.target.value)}
+    //       placeholder="Title"
+    //     />
+    //   </Form.Field>
+
+    //   <Form.Field>
+    //     <label>Subtitle</label>
+    //     <input
+    //       name="bsubtitle"
+    //       value={subtitle}
+    //       onChange={(e) => setSubtitle(e.target.value)}
+    //       placeholder="Subtitle"
+    //     />
+    //   </Form.Field>
+
+    //   <Form.Field>
+    //     <label>Author</label>
+    //     <input
+    //       name="bauthor"
+    //       value={author}
+    //       onChange={(e) => setAuthor(e.target.value)}
+    //       placeholder="Author"
+    //     />
+    //   </Form.Field>
+
+    //   <Form.Field>
+    //     <label>ISBN</label>
+    //     <input
+    //       name="bisbn"
+    //       value={isbn}
+    //       onChange={(e) => setISBN(e.target.value)}
+    //       placeholder="ISBN"
+    //     />
+    //   </Form.Field>
+
+    //   <Form.Field>
+    //     <label>Published</label>
+    //     <input
+    //       name="bpublished"
+    //       value={published.substring(0, 4)}
+    //       onChange={(e) => setPublished(e.target.value)}
+    //       placeholder="Published"
+    //     />
+    //   </Form.Field>
+
+    //   <Form.Field>
+    //     <label>Publisher</label>
+    //     <input
+    //       name="bpublisher"
+    //       value={publisher}
+    //       onChange={(e) => setPublisher(e.target.value)}
+    //       placeholder="Publisher"
+    //     />
+    //   </Form.Field>
+
+    //   <Form.Field>
+    //     <label>Image</label>
+    //     <input
+    //       name="bimage"
+    //       value={image}
+    //       onChange={(e) => setImage(e.target.value)}
+    //       placeholder="Image"
+    //     />
+    //   </Form.Field>
+
+    //   <Form.Field>
+    //     <label>Pages</label>
+    //     <input
+    //       name="bpages"
+    //       value={pages}
+    //       onChange={(e) => setPages(e.target.value)}
+    //       placeholder="Pages"
+    //     />
+    //   </Form.Field>
+
+    //   <Form.Field>
+    //     <label>Description</label>
+    //     <input
+    //       name="bdescription"
+    //       value={description}
+    //       onChange={(e) => setDescription(e.target.value)}
+    //       placeholder="Description"
+    //     />
+    //   </Form.Field>
+
+    //   <Form.Field>
+    //     <label>Website</label>
+    //     <input
+    //       name="bwebsite"
+    //       value={website}
+    //       onChange={(e) => setWebsite(e.target.value)}
+    //       placeholder="Website"
+    //     />
+    //   </Form.Field>
+
+    //   <Button type="submit" onClick={sendDataToAPI}>
+    //     Update
+    //   </Button>
+    // </Form>
   );
 }
